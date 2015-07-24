d3f5c7ef77f6c538cd6f78625c7f7e2946608291
Anthony Eden <me@anthonyeden.com>
2015-07-24 15:23:54 +0200

    Just playing some more.

--- example.rb
+++ example.rb
@@ -11,11 +11,33 @@ while(true)
   sleep(1)
 end
 
+# slow up
+all_off
+while(true)
+  c = Color.new(rand(255), rand(255), rand(255))
+  puts "color: #{c.inspect}"
+  0.upto(140) do |i|
+    on(i, c)
+    sleep(0.05)
+  end
+  sleep(2)
+end
+
+# slow back and forth
+all_off
+while(true)
+  0.upto(140) { |i| on(i, RED); sleep(0.05) }
+  0.upto(140) { |i| on(i, BLUE); sleep(0.05) }
+  140.downto(0) { |i| on(i, RED); sleep(0.05) }
+  140.downto(0) { |i| on(i, BLUE); sleep(0.05) }
+  sleep(1)
+end
+
 # random
 
 all_off
 while(true)
-  1.upto(140) do |n|
+  0.upto(140) do |n|
     lights(target: 0, i: n, r: rand(255), g: rand(255), b: rand(255))
   end
   sleep(5)
@@ -23,7 +45,7 @@ end
 
 # black, red, yellow
 all_off
-1.upto(140) do |n|
+0.upto(140) do |n|
   case n % 3
     when 0 then
       lights(target: 0, i: n, r: 0, g: 0, b: 0)
@@ -39,7 +61,7 @@ end
 
 # Viva la France!
 all_off
-1.upto(140) do |n|
+0.upto(140) do |n|
   case n % 3
     when 0 then
       on(n, Color.new(0, 0, 255))
